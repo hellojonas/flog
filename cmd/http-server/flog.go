113bf915f35e8b3ed90cff96234981f2f1b2b972
Hello Jonas <hellojonashp@gmail.com>
2024-04-08 01:59:40 +0200

    fix identing on http server

--- cmd/http-server/flog.go
+++ cmd/http-server/flog.go
@@ -16,16 +16,16 @@ func main() {
 	bufSize := flag.Int("bufsize", 100, "Log entries buffers size")
 	port := flag.Int("port", 8080, "Port to listen")
 	logDir := flag.String("log-dir", "", "Root log directory")
-    poll := flag.Int("poll", 700, "Poll interval in ms")
-    flag.Parse()
-    addr := ":" + strconv.FormatInt(int64(*port), 10)
+	poll := flag.Int("poll", 700, "Poll interval in ms")
+	flag.Parse()
+	addr := ":" + strconv.FormatInt(int64(*port), 10)
 	store := make(chan logger.Entry, *bufSize)
 
-    fmt.Println(*bufSize, *port, *logDir, addr)
+	fmt.Println(*bufSize, *port, *logDir, addr)
 
 	if logDir == nil || *logDir == "" {
 		home := os.Getenv("HOME")
-        p := (filepath.Join(home, ".flog", "logs"))
+		p := (filepath.Join(home, ".flog", "logs"))
 		logDir = &p
 	}
 
@@ -44,4 +44,3 @@ func main() {
 	log.Println("application listening on ", addr)
 	log.Fatal(http.ListenAndServe(addr, server))
 }
-
