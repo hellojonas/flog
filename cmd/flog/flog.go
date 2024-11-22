c6e8023d9cff8c59cdc2bcc25b6e8599b02a02c9
Hello Jonas <hellojonashp@gmail.com>
2024-11-22 23:39:50 +0200

    restart

--- cmd/flog/flog.go
+++ cmd/flog/flog.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"github.com/hellojonas/flog/pkg/tcp"
-)
-
 func main() {
-	server := tcp.NewTCPServer("127.0.0.1", 4004)
-	err := server.Listen()
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer server.Close()
-
-	server.StartAccept()
-
-    if err != nil {
-        panic(err)
-    }
 }
